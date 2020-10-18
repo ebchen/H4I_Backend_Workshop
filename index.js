b0890273ff3735938ec145ecff10264ca40e3a0e
Abhishek Pandya <abhishekp213@hotmail.com>
2020-10-18 14:47:30 -0400

    connected

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-var MongoClient = require('mongodb').MongoClient
-connection_string = 'mongodb+srv://abhi:abhi1234@cluster0.tbqxb.mongodb.net/<dbname>?retryWrites=true&w=majority'
+
+const MongoClient = require('mongodb').MongoClient;
+const connection_string = "mongodb+srv://abhi:abhi1234@cluster0.tbqxb.mongodb.net/todolist?retryWrites=true&w=majority";
 
 MongoClient.connect(connection_string, function (err, client) {
         if (err) throw err
 
         var db = client.db('todolist')
 
-        db.collection('tasks').find().toArray(function (err, result) {
-            if (err) throw err
-            console.log(result)
-        })
-    })
+        db.collection('tasks')
+})
 
 app.post('/addtask', function(req, res) {
     MongoClient.connect(connection_string, function (err, client) {
@@ -31,7 +29,7 @@ app.post('/addtask', function(req, res) {
 })
 
 app.get('/gettasks', function(req, res) {
-    MongoClient.connect('mongodb://localhost:27017/todolist', function (err, client) {
+    MongoClient.connect(connection_string, function (err, client) {
         if (err) throw err
 
         var db = client.db('todolist')
@@ -41,4 +39,8 @@ app.get('/gettasks', function(req, res) {
             console.log(result)
         })
     })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
 })
